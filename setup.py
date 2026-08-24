1a64a21030c2c68356416a099c1ccbdf35bc6e09
geralt14 <wmkkkk@126.com>
2026-08-24 15:09:23 +0100

    update version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="enzywizard-mut-batch",
-    version=__version__,                     # dynamically read from version.py (1.0.1)
+    version=__version__,                     # dynamically read from version.py
     author="bioinfbrad",
     description=(
         "Run paired EnzyWizard analysis workflows for a wild-type protein and its mutant, "
